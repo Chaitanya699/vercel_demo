e4ac1f786c08d84dcde0c9c5a07371077be9ad47
Chaitanya699 <chaitanyakhairnar143@gmail.com>
2024-07-28 17:47:15 +0530

    fourth commit

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ import express from "express";
 const app = express();
 const port = 9000;
 
-app.use("/", (req,res) => {
-    res.send("Hello, World!");
-})
+// Middleware to parse JSON data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(9000, () => {
+// GET route
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+// POST route to take input and send output
+app.post("/user", (req, res) => {
+    const { input } = req.body; // Assuming the input is sent as JSON
+    const output = `You sent: ${input}`;
+    res.send(output);
+});
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
